eff80f1040cce9f3382bb832e6a114319452971e
Roei <roei.keisar@gmail.com>
2023-01-25 15:45:29 +0000

    if u wont work version/6.0

--- tag_calc.sh
+++ tag_calc.sh
@@ -11,15 +11,12 @@ Version=$(git tag --list | grep $MAJOR.$MINOR)
 if [ "${Version}" = "" ];then
 Version="${MAJOR}.${MINOR}.1"
 else
-
 BDIKA=$(git tag --list | grep $MAJOR.$MINOR |  tail -n1)
 MAJOR=$(echo $BDIKA | cut -d '/' -f2 | cut -d '.' -f1)
 MINOR=$(echo $BDIKA | cut -d '/' -f2 | cut -d '.' -f2)
 PATCH=$(echo $BDIKA | cut -d '/' -f2 | cut -d '.' -f3)
-
 NEW_PATCH=`expr $PATCH + 1`
 Version="${MAJOR}.${MINOR}.${NEW_PATCH}"
-
 fi
 
 echo $Version
