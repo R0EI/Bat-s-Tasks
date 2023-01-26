47b5316ffc68087ddb53a370da51de73c57698cc
Roei <roei.keisar@gmail.com>
2023-01-26 08:58:12 +0000

    it will work version/5.0

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 TAG=$1
 export TAG
 cd myapp
+aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.eu-west-3.amazonaws.com
 docker rmi $(docker images -aq)
 docker-compose -f prod-docker-compose.yml build --no-cache
 docker-compose -f prod-docker-compose.yml up -d
